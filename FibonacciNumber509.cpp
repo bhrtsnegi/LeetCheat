ddde4aba8f305bcfdecf29509e4df161adb2a0af
Bharat Negi <1bhrtsnegi@gmail.com>
2023-01-20 10:29:43 +0530

    Create FibonacciNumber509.cpp
    Leetcode 509th problem

---
+++ FibonacciNumber509.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int fib(int n) {
+            int n1=0, n2=1, temp;
+    if(n==0)
+    {
+        return 0;
+    }
+    else if(n==1)
+    {
+        return 1;
+    }
+    else
+    {
+        for(int i=1; i!=n; i++)
+        {
+            temp= n2+n1;
+            n1=n2;
+            n2= temp;
+            if(i==n-1)
+            {
+                return n2;
+            }
+        }
+    }
+    return 0;
+    }
+};
