c7b767077069a6680d0b38ade20548cd2c72ff46
Bharat Negi <1bhrtsnegi@gmail.com>
2023-02-08 22:42:48 +0530

    Update PowerOfFour342.cpp

--- PowerOfFour342.cpp
+++ PowerOfFour342.cpp
@@ -4,6 +4,7 @@ Given an integer n, return true if it is a power of four. Otherwise, return fals
 An integer n is a power of four, if there exists an integer x such that n == 4x.
 */
 
+
 class Solution {
 public:
     bool isPowerOfFour(int n) {
