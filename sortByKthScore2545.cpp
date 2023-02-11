e7ded8ecbabd5394a71a61e858fe3c7eaf3f3422
Bharat Negi <1bhrtsnegi@gmail.com>
2023-02-11 15:32:15 +0530

    Update sortByKthScore2545.cpp
    Code Updated

--- sortByKthScore2545.cpp
+++ sortByKthScore2545.cpp
@@ -3,30 +3,28 @@ There is a class with m students and n exams. You are given a 0-indexed m x n in
 where each row represents one student and score[i][j] denotes the score the ith student got in the jth exam. 
 The matrix score contains distinct integers only.
 */
-
 class Solution {
 public:
     vector<vector<int>> sortTheStudents(vector<vector<int>>& score, int k) {
         vector<vector<int>> result;
-        int max=0, point, i=0;
-        for(int j=0; j<size(score); j++)
+        int max=0, point, j=0;
+        for(int m=0; m<score[0].size(); m++)
         {
-            for(int i=0; i<size(score); i++)
+            for(int i=0; i<score[0].size(); i++)
             {
-                if(max<score[i][k])
+                if(score[i][k]>max)
                 {
                     max=score[i][k];
-                    point= i;
+                    point=i;
                 }
             }
-            
-                for(int j=0; i<score[i].size(); j++)
-                {
-                    result[i][j]=score[point][j];
-                }
-                i++;
-            score[j][point]=0;
+            for(int i=0; size(score); i++)
+            {
+                result[j][i]=score[point][i];
+            }
+            score[point][k]=0;
             max=0;
+            j++;
         }
         return result;
     }
