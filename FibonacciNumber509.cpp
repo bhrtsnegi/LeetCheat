ba4aae4e93e0bc91dc160797e333a3a0771716c8
Bharat Negi <1bhrtsnegi@gmail.com>
2023-02-15 14:20:20 +0530

    Update FibonacciNumber509.cpp
    Question Updated

--- FibonacciNumber509.cpp
+++ FibonacciNumber509.cpp
@@ -1,4 +1,5 @@
-// Question need to be updated here
+// The Fibonacci numbers, commonly denoted F(n) form a sequence, called the Fibonacci sequence, such that each number is the sum of the two preceding ones, starting from 0 and 1. That is,
+
 // Question number 509 in leetcode
 
 class Solution {
