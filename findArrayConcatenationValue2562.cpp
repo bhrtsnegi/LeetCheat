911ad1ddaf071f97f0bb8b52e8a3feb91f5362b5
Bharat Negi <1bhrtsnegi@gmail.com>
2023-02-23 01:17:20 +0530

    Create findArrayConcatenationValue2562.cpp
    Program done

---
+++ findArrayConcatenationValue2562.cpp
@@ -0,0 +1,35 @@
+/*
+You are given a 0-indexed integer array nums.
+
+The concatenation of two numbers is the number formed by concatenating their numerals.
+
+For example, the concatenation of 15, 49 is 1549.
+The concatenation value of nums is initially equal to 0. Perform this operation until nums becomes empty:
+
+If there exists more than one number in nums, pick the first element and last element in nums respectively and add the value of their concatenation to the concatenation value of nums, then delete the first and last element from nums.
+If one element exists, add its value to the concatenation value of nums, then delete it.
+Return the concatenation value of the nums.
+*/
+class Solution {
+public:
+    long long findTheArrayConcVal(vector<int>& nums) {
+        long ans=0;
+        int num= size(nums);
+        string s1;
+        for(int i=0; i<num; i++)
+        {
+            if((num-1)!=i)
+            {
+                s1= to_string(nums[i]) + to_string(nums[num-1]);
+                num=num-1;
+                ans=ans+ stoi(s1);
+            }
+            else
+            {
+                num=num-1;
+                ans=ans+ nums[i];
+            }
+        }
+        return ans;
+    }
+};
