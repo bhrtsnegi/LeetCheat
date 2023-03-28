09fab59d286127b60f5d2068b9d36d43e01c28d2
Bharat Negi <1bhrtsnegi@gmail.com>
2023-03-28 23:41:48 +0530

    Update FizzBuzz412.cpp

--- FizzBuzz412.cpp
+++ FizzBuzz412.cpp
@@ -1,6 +1,5 @@
 /*
 Given an integer n, return a string array answer (1-indexed) where:
-
 answer[i] == "FizzBuzz" if i is divisible by 3 and 5.
 answer[i] == "Fizz" if i is divisible by 3.
 answer[i] == "Buzz" if i is divisible by 5.
