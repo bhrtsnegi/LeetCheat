52fbfc8743efda4320db0d9dd79cb75f4ca25b4e
Bharat Negi <1bhrtsnegi@gmail.com>
2023-04-06 22:47:53 +0530

    Update convertTemperature2469.cpp

--- convertTemperature2469.cpp
+++ convertTemperature2469.cpp
@@ -1,7 +1,5 @@
 /*You are given a non-negative floating point number rounded to two decimal places celsius, that denotes the temperature in Celsius.
-
 You should convert Celsius into Kelvin and Fahrenheit and return it as an array ans = [kelvin, fahrenheit].
-
 Return the array ans. Answers within 10-5 of the actual answer will be accepted.*/
 
 //Solution here
