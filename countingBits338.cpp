8bc923b3d4de15582259812c1a4f232e5cbd9d90
Bharat Negi <1bhrtsnegi@gmail.com>
2023-04-20 16:21:00 +0530

    Code Updated

--- countingBits338.cpp
+++ countingBits338.cpp
@@ -1,3 +1,24 @@
 /*
 
 */
+
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        vector<int> ans;
+        int count=0;
+        for(int i=0; i<=n; i++)
+        {
+            int k=i;
+            while(k!=0)
+            {
+                if(k%2==1)
+                    count++;
+                k/=2;
+            }
+            ans.push_back(count);
+            count=0;
+        }
+        return ans;
+    }
+};
