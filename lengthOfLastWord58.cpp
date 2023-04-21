70ebfbc4908a7bf96b2f36e3c24d48d632e8d15b
Bharat Negi <1bhrtsnegi@gmail.com>
2023-04-21 10:07:00 +0530

    Solved

--- lengthOfLastWord58.cpp
+++ lengthOfLastWord58.cpp
@@ -1,9 +1,6 @@
 /*
 Given a string s consisting of words and spaces, return the length of the last word in the string.
-
-A word is a maximal 
-substring
- consisting of non-space characters only.
+A word is a maximal substring consisting of non-space characters only.
 */
 
 class Solution {
