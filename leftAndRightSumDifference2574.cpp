b7c75e153491221e9bc1d0f9a4e89b33d75b4e1b
Bharat Negi <1bhrtsnegi@gmail.com>
2023-04-24 16:18:41 +0530

    Solved

--- leftAndRightSumDifference2574.cpp
+++ leftAndRightSumDifference2574.cpp
@@ -9,3 +9,20 @@ leftSum[i] is the sum of elements to the left of the index i in the array nums. 
 rightSum[i] is the sum of elements to the right of the index i in the array nums. If there is no such element, rightSum[i] = 0.
 Return the array answer.
 */
+
+class Solution {
+public:
+    vector<int> leftRigthDifference(vector<int>& nums) {
+        int lsum=0;
+        int rsum=0;
+        for(int i=0; i<nums.size(); i++) rsum+=nums[i];
+        vector<int> answer;
+        for(int i=0; i<nums.size(); i++)
+        {
+            rsum-=nums[i];
+            answer.push_back(abs(lsum-rsum));
+            lsum+=nums[i];
+        }
+        return answer;
+    }
+};
