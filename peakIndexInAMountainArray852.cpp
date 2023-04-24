5a3f40a113baf5428754bc821a5bab5869918224
Bharat Negi <1bhrtsnegi@gmail.com>
2023-04-24 10:22:15 +0530

    Solved

--- peakIndexInAMountainArray852.cpp
+++ peakIndexInAMountainArray852.cpp
@@ -9,3 +9,15 @@ Given a mountain array arr, return the index i such that arr[0] < arr[1] < ... <
 
 You must solve it in O(log(arr.length)) time complexity.
 */
+
+class Solution {
+public:
+    int peakIndexInMountainArray(vector<int>& arr) {
+        for(int i=0; i<arr.size(); i++)
+        {
+            if(arr[i]>arr[i+1])
+                return i;
+        }
+        return 0;
+    }
+};
