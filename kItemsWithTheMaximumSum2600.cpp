6ab385c35ce9f9665678ab32b7ab725a12bc29cd
Bharat Negi <1bhrtsnegi@gmail.com>
2023-04-24 16:26:01 +0530

    Solved

--- kItemsWithTheMaximumSum2600.cpp
+++ kItemsWithTheMaximumSum2600.cpp
@@ -9,4 +9,24 @@ The bag initially contains:
 numOnes items with 1s written on them.
 numZeroes items with 0s written on them.
 numNegOnes items with -1s written on them.
-We want to pick exactly k items among the available items. Return the maximum possible sum of numbers written on the items.*/
+We want to pick exactly k items among the available items. Return the maximum possible sum of numbers written on the items.
+*/
+
+
+class Solution {
+public:
+    int kItemsWithMaximumSum(int numOnes, int numZeros, int numNegOnes, int k) {
+        int sum=0;
+        for(int i=0; i<k; i++)
+        {
+            if(numOnes>0)
+            {
+                numOnes--;
+                sum+=1;
+            }
+            else if(numZeros>0) numZeros--;
+            else sum-=1;
+        }
+        return sum;
+    }
+};
