32953dac611c1d5650d488d12a204d0f7c8c09a4
Bharat Negi <1bhrtsnegi@gmail.com>
2023-04-26 16:46:31 +0530

    Solved

--- missingNumber268.cpp
+++ missingNumber268.cpp
@@ -1,3 +1,18 @@
 /*
 Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
 */
+
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int val;
+        for(int i=0; i<nums.size(); i++)
+        {
+            if(i!=nums[i])
+                return i;
+            val=i;
+        }
+        return val+1;
+    }
+};
