d4d19a3b399e42ee699a3a13a41d9dbdcd4f8990
Bharat Negi <1bhrtsnegi@gmail.com>
2023-05-01 18:20:19 +0530

    question Updated

--- leetContest/numberEvenOddBits2595.cpp
+++ leetContest/numberEvenOddBits2595.cpp
@@ -1,3 +1,6 @@
 /*
-
+You are given a positive integer n.
+Let even denote the number of even indices in the binary representation of n (0-indexed) with value 1.
+Let odd denote the number of odd indices in the binary representation of n (0-indexed) with value 1.
+Return an integer array answer where answer = [even, odd].
 */
