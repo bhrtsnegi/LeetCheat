add43e0814497fafcac84411dcd0afc41f96abbd
Bharat Negi <1bhrtsnegi@gmail.com>
2023-05-08 23:35:16 +0530

    Solved

--- matrixDiagonalSum1572.cpp
+++ matrixDiagonalSum1572.cpp
@@ -2,3 +2,18 @@
 Given a square matrix mat, return the sum of the matrix diagonals.
 Only include the sum of all the elements on the primary diagonal and all the elements on the secondary diagonal that are not part of the primary diagonal.
 */
+
+class Solution {
+public:
+    int diagonalSum(vector<vector<int>>& mat) {
+        int sum=0,j;
+        for(int i=0;i<mat.size();i++)
+        {
+            j=mat.size()-1-i;
+            sum+=mat[i][i]+mat[i][j];
+            if(i==j)
+                sum-=mat[i][i];
+        }
+        return sum;
+    }
+};
