cd7c0feb756473060f67b992dbf4337119e10593
Bharat Negi <1bhrtsnegi@gmail.com>
2023-05-13 21:11:36 +0530

    Solved

--- leetContest/sumInAMatrix6367.cpp
+++ leetContest/sumInAMatrix6367.cpp
@@ -5,3 +5,27 @@ From each row in the matrix, select the largest number and remove it. In the cas
 Identify the highest number amongst all those removed in step 1. Add that number to your score.
 Return the final score.
 */
+
+class Solution {
+public:
+    int matrixSum(vector<vector<int>>& nums) {
+        int max=0,score=0;
+        int n=nums[0].size()-1;
+    while(n>=0)
+    {
+        for(int i=0; i<nums.size(); i++)
+        {
+            if(n==nums[0].size()-1)
+            sort(nums[i].begin(), nums[i].end());
+            if(max<nums[i][n])
+            {
+                max=nums[i][n];
+            }
+        }
+        score+=max;
+        max=0;
+        n--;
+    }
+       return score; 
+    }
+};
