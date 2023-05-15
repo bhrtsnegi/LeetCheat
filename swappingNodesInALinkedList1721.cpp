e7f7c3fa69436dc14b3268f8d5541164e4796ef2
Bharat Negi <1bhrtsnegi@gmail.com>
2023-05-15 15:34:50 +0530

    Update swappingNodesInALinkedList1721.cpp

--- swappingNodesInALinkedList1721.cpp
+++ swappingNodesInALinkedList1721.cpp
@@ -1,3 +1,4 @@
 /*
-
+You are given the head of a linked list, and an integer k.
+Return the head of the linked list after swapping the values of the kth node from the beginning and the kth node from the end (the list is 1-indexed).
 */
