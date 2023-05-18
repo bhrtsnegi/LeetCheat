031b2bf563318ca19f0d38163bc894208309130b
Bharat Negi <1bhrtsnegi@gmail.com>
2023-05-18 18:38:42 +0530

    Update minimumNumberOfVerticesToReachAllNodes1557.cpp

--- graph/minimumNumberOfVerticesToReachAllNodes1557.cpp
+++ graph/minimumNumberOfVerticesToReachAllNodes1557.cpp
@@ -1,3 +1,28 @@
 /*
 
 */
+
+#include<set>
+class Solution {
+public:
+    vector<int> findSmallestSetOfVertices(int n, vector<vector<int>>& edges) {
+        //created a boolean array where every value is false at beginning
+        vector<bool> check (n,false);
+
+        
+        for(int i = 0; i < edges.size(); i++)
+        {
+            check[edges[i][1]] = true;
+        }
+
+        vector<int> result;
+        for(int i = 0; i < n; i++)
+        {
+            if(check[i] == false)
+            {
+                result.push_back(i);
+            }
+        }
+        return result;
+    }
+};
