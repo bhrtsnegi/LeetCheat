55368567d22eda846d47939215a54f32e6639509
Bharat Negi <1bhrtsnegi@gmail.com>
2023-05-21 10:11:49 +0530

    Solved

--- deleteTheMiddleOfALinkedList2095.cpp
+++ deleteTheMiddleOfALinkedList2095.cpp
@@ -4,3 +4,39 @@ The middle node of a linked list of size n is the ⌊n / 2⌋th node from the st
 where ⌊x⌋ denotes the largest integer less than or equal to x.
 For n = 1, 2, 3, 4, and 5, the middle nodes are 0, 1, 1, 2, and 2, respectively.
 */
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* deleteMiddle(ListNode* head) {
+        ListNode* ptr=head;
+        ListNode* preptr;
+        int count=0;
+        while(ptr)
+        {
+            count++;
+            ptr=ptr->next;
+        }
+        ptr=head;
+        if(count==1)
+            return head=ptr->next;
+        count=(count/2)+1;
+        while(count>1)
+        {
+            count--;
+            preptr=ptr;
+            ptr=ptr->next;
+        }
+        preptr->next=ptr->next;
+        return head;
+    }
+};
