8c79f265c4d47c6392fb4480ff72ab95712db38a
Bharat Negi <1bhrtsnegi@gmail.com>
2023-06-09 11:55:35 +0530

    Solved: Product sales Analysis 1

---
+++ sql/1068.sql
@@ -0,0 +1,40 @@
+/*
+Table: Sales
+
++-------------+-------+
+| Column Name | Type  |
++-------------+-------+
+| sale_id     | int   |
+| product_id  | int   |
+| year        | int   |
+| quantity    | int   |
+| price       | int   |
++-------------+-------+
+(sale_id, year) is the primary key of this table.
+product_id is a foreign key to Product table.
+Each row of this table shows a sale on the product product_id in a certain year.
+Note that the price is per unit.
+ 
+
+Table: Product
+
++--------------+---------+
+| Column Name  | Type    |
++--------------+---------+
+| product_id   | int     |
+| product_name | varchar |
++--------------+---------+
+product_id is the primary key of this table.
+Each row of this table indicates the product name of each product.
+ 
+
+Write an SQL query that reports the product_name, year, and price for each sale_id in the Sales table.
+
+Return the resulting table in any order.
+*/
+
+select p.product_name, s.year, s.price
+from product p
+join
+sales s on p.product_id=s.product_id
+where sale_id is not null;
