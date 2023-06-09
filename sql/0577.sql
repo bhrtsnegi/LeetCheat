0ff200f0d153b5a78b34c135f3e62dcc89b8d4c2
Bharat Negi <1bhrtsnegi@gmail.com>
2023-06-09 14:45:51 +0530

    Solved: Employee Bonus

---
+++ sql/0577.sql
@@ -0,0 +1,39 @@
+/*
+Table: Employee
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| empId       | int     |
+| name        | varchar |
+| supervisor  | int     |
+| salary      | int     |
++-------------+---------+
+empId is the primary key column for this table.
+Each row of this table indicates the name and the ID of an employee in addition to their salary and the id of their manager.
+ 
+
+Table: Bonus
+
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| empId       | int  |
+| bonus       | int  |
++-------------+------+
+empId is the primary key column for this table.
+empId is a foreign key to empId from the Employee table.
+Each row of this table contains the id of an employee and their respective bonus.
+ 
+
+Write an SQL query to report the name and bonus amount of each employee with a bonus less than 1000.
+
+Return the result table in any order.
+*/
+
+# Write your MySQL query statement below
+select e.name ,b.bonus 
+from employee e
+left join bonus b
+using(empId)
+where b.bonus<1000 or b.bonus is NULL;
