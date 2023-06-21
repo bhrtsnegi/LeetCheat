a047f1f419a3448baefc28010e051fcf0ddf13a8
Bharat Negi <1bhrtsnegi@gmail.com>
2023-06-21 18:12:23 +0530

    Update 1005.cpp

--- 1005.cpp
+++ 1005.cpp
@@ -6,17 +6,23 @@ You should apply this process exactly k times. You may choose the same index i m
 
 Return the largest possible sum of the array after modifying it in this way.
 */
+
 class Solution {
 public:
     int largestSumAfterKNegations(vector<int>& nums, int k) {
         sort(nums.begin(), nums.end());
         for(int i=0;k>0; i++)
         {
+            if(i==nums.size()) i=0;
             if(nums[i]<0 && nums.size()>i)
+            {
                 nums[i]=-nums[i];
+                k--;
+            }
+                
             else if(nums[i]==0 && nums.size()>i)
                 k=0;
-            else if(nums[i]>0 || i==nums.size())
+            else if(nums[i]>0)
             {
                 sort(nums.begin(), nums.end());
                 while(k!=0)
@@ -26,7 +32,6 @@ public:
                 }
                 break;
             }
-            k--;
         }
         int sum=0;
         for(int i=0; i<nums.size(); i++)
