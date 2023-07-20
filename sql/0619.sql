bce058d5c2678ea4410c1784c099a367a4aa2fbf
Bharat Negi <1bhrtsnegi@gmail.com>
2023-07-20 18:06:44 +0530

    Solved: Biggest Single Number

---
+++ sql/0619.sql
@@ -0,0 +1,22 @@
+/*
+Table: MyNumbers
+
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| num         | int  |
++-------------+------+
+There is no primary key for this table. It may contain duplicates.
+Each row of this table contains an integer.
+ 
+
+A single number is a number that appeared only once in the MyNumbers table.
+
+Write an SQL query to report the largest single number. If there is no single number, report null.
+
+
+*/
+
+select max(num) as num 
+  from
+    (select num from MyNumbers group by num having count(num)=1) as t
