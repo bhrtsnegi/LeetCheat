597eca65cd3464e12076a16721ee85e6d7bb7cdf
Bharat Negi <1bhrtsnegi@gmail.com>
2023-08-07 20:23:22 +0530

    Solved: Range Sum Query

---
+++ 0303.cpp
@@ -0,0 +1,25 @@
+/*
+Given an integer array nums, handle multiple queries of the following type:
+
+Calculate the sum of the elements of nums between indices left and right inclusive where left <= right.
+Implement the NumArray class:
+
+NumArray(int[] nums) Initializes the object with the integer array nums.
+int sumRange(int left, int right) Returns the sum of the elements of nums between indices left and right inclusive 
+(i.e. nums[left] + nums[left + 1] + ... + nums[right]).
+*/
+
+class NumArray {
+public:
+    vector<int>arr;
+    NumArray(vector<int>& nums) {
+        arr=nums;
+    }
+    
+    int sumRange(int left, int right) {
+        int sum=0;
+        for(int i=left;i<=right;i++)
+            sum+=arr[i];
+        return sum;
+    }
+};
