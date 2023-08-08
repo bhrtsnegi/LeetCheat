9ebfd88ea4010a25976feb41a14d247fed23db14
Bharat Negi <1bhrtsnegi@gmail.com>
2023-08-08 22:45:37 +0530

    Solved: Top travellers

---
+++ sql/1407.sql
@@ -0,0 +1,38 @@
+/*
+Table: Users
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| id            | int     |
+| name          | varchar |
++---------------+---------+
+id is the column with unique values for this table.
+name is the name of the user.
+ 
+
+Table: Rides
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| id            | int     |
+| user_id       | int     |
+| distance      | int     |
++---------------+---------+
+id is the column with unique values for this table.
+user_id is the id of the user who traveled the distance "distance".
+ 
+
+Write a solution to report the distance traveled by each user.
+
+Return the result table ordered by travelled_distance in descending order, if two or more users traveled the same distance, order them by their name in ascending order.
+*/
+
+SELECT u.name,
+    IFNULL (SUM(r.distance),0) AS travelled_distance
+FROM Users AS u
+LEFT JOIN Rides AS r
+ON u.id=r.user_id
+GROUP BY u.name
+ORDER BY travelled_distance DESC, name;
