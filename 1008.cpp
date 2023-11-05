bdc698355dfb5f6986ecf44173d2a19309739116
Bharat Negi <1bhrtsnegi@gmail.com>
2023-11-05 08:57:21 +0530

    Solved: BST from Preorder

---
+++ 1008.cpp
@@ -0,0 +1,38 @@
+/*
+Given an array of integers preorder, which represents the preorder traversal of a BST (i.e., binary search tree), construct the tree and return its root.
+
+It is guaranteed that there is always possible to find a binary search tree with the given requirements for the given test cases.
+
+A binary search tree is a binary tree where for every node, any descendant of Node.left has a value strictly less than Node.val, and any descendant of Node.right has a value strictly greater than Node.val.
+
+A preorder traversal of a binary tree displays the value of the node first, then traverses Node.left, then traverses Node.right.
+*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* bstFromPreorder(vector<int>& preorder) {
+        int i = 0;
+        return solveBlock(preorder, i, INT_MAX);
+    }
+
+    TreeNode* solveBlock(vector<int>& arr, int& i, int bound)
+    {
+        if(i == arr.size() || arr[i]>bound) return NULL;
+        TreeNode* root = new TreeNode(arr[i++]);
+        root->left = solveBlock(arr, i, root->val);
+        root->right = solveBlock(arr, i, bound);
+
+        return root;
+    }
+};
