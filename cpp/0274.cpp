8539ebf070c4c82967aac9079ad0b183fb239138
Bharat Negi <1bhrtsnegi@gmail.com>
2024-04-09 08:48:48 +0530

    Solved: H-Index

---
+++ cpp/0274.cpp
@@ -0,0 +1,17 @@
+/*
+Given an array of integers citations where citations[i] is the number of citations a researcher received for their ith paper, return the researcher's h-index.
+
+According to the definition of h-index on Wikipedia: 
+The h-index is defined as the maximum value of h such that the given researcher has published at least h papers that have each been cited at least h times.
+*/
+
+class Solution {
+public:
+    int hIndex(vector<int>& citations) {
+        sort(citations.begin(), citations.end());
+        int n = citations.size(), i;
+        for(i = 1; i <= n; i++)
+            if(citations[n-i] < i) break;
+        return i-1;
+    }
+};
